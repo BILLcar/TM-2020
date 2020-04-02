16e4a0effdfa76ffa6b95b2a9925de02293b528c
BILLcar <61700814+BILLcar@users.noreply.github.com>
2020-04-02 18:17:25 +0100

    aula5
    miguel-22063

--- Aula 5/PongGame-Miguel-22063/Assets/Scripts/Paddle.cs
+++ Aula 5/PongGame-Miguel-22063/Assets/Scripts/Paddle.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pddle : MonoBehaviour
+public class paddle : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public Rigidbody2D rb;
+
+    private Vector3 startPosition;
+
+    private float movement;
+
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movement = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
     }
 }
+
---
+++ Aula 5/PongGame-Miguel-22063/Assets/Scripts/goal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ballTag"))
+        {
+            GameObject.Find("gameManagerObject").GetComponent<manager>().Player1Scored();
+        }
+    }
+}
